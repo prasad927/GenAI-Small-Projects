c97f41d314ce6c184cb035698cf6513682332861
prasad927 <chaudhariprasad69@gmail.com>
2025-05-24 13:57:10 +0530

    added logic

---
+++ project1_instant_gratification.py
@@ -0,0 +1,81 @@
+import os
+import requests
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+from IPython.display import Markdown, display
+from openai import OpenAI
+
+
+
+load_dotenv(overide=True)
+api_key = os.getenv('OPENAI_API_KEY')
+
+if not api_key:
+    print("No API key was found")
+elif not api_key.startswith("sk-proj-"):
+    print("An API key was found, but it doesn't start sk-proj-; please check you're using the right key")
+elif api_key.strip() != api_key:
+    print("An API key was found, but it looks like it might have space or tab characters at the start or end please remove them")
+else:
+    print("API key found and looks good so far!")
+
+
+openai = OpenAI()
+
+headers = {
+
+}
+
+class Website:
+
+    def __init__(self,url):
+        """
+        Create this Website object from the given url using the BeautifulSoup library
+        """
+        self.url = url
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.title = soup.title.string if soup.title else "No title found"
+        for irrelevant in soup.body(["script", "style", "img", "input"]):
+            irrelevant.decompose()
+        self.text = soup.body.get_text(separator="\n", strip=True)
+
+
+system_prompt = "You are an assistant that analyzes the contents of a website \
+and provides a short summary, ignoring text that might be navigation related. \
+Respond in markdown."
+
+
+def user_prompt_for(website):
+    user_prompt = f"You are looking at a website titled {website.title}"
+    user_prompt += "\nThe contents of this website is as follows; \
+please provide a short summary of this website in markdown. \
+If it includes news or announcements, then summarize these too.\n\n"
+    user_prompt += website.text
+    return user_prompt
+
+
+def messages_for(website):
+    return [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt_for(website)}
+    ]
+
+
+def summarize(url):
+    website = Website(url)
+    response = openai.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = messages_for(website)
+    )
+    return response.choices[0].message.content
+
+
+def display_summary(url):
+    summary = summarize(url)
+    display(Markdown(summary))
+
+
+display_summary("https://edwarddonner.com")
+# display_summary("https://cnn.com")
+# display_summary("https://anthropic.com")
